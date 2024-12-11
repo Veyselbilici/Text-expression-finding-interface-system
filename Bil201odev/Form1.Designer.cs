7e36b44a926042e2e3f148b7590da7978b03581e
Veysel <128748791+Veyselbilici@users.noreply.github.com>
2024-12-11 12:56:43 +0300

    Add files via upload

---
+++ Bil201odev/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace Bil201odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Aranacakifade = new System.Windows.Forms.TextBox();
+            this.Metin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(2, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 447);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.PeachPuff;
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.checkBox3);
+            this.tabPage1.Controls.Add(this.checkBox2);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.Aranacakifade);
+            this.tabPage1.Controls.Add(this.Metin);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(791, 418);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ana Ekran";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Location = new System.Drawing.Point(336, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "İleri>";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.Location = new System.Drawing.Point(336, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Çalıştır";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(499, 258);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(84, 21);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Karakter";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(369, 258);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(57, 21);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Sayı";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(244, 258);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Metin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Aranacakifade
+            // 
+            this.Aranacakifade.Location = new System.Drawing.Point(244, 206);
+            this.Aranacakifade.Name = "Aranacakifade";
+            this.Aranacakifade.Size = new System.Drawing.Size(340, 22);
+            this.Aranacakifade.TabIndex = 3;
+            this.Aranacakifade.TextChanged += new System.EventHandler(this.Aranacakifade_TextChanged);
+            // 
+            // Metin
+            // 
+            this.Metin.Location = new System.Drawing.Point(244, 73);
+            this.Metin.Multiline = true;
+            this.Metin.Name = "Metin";
+            this.Metin.Size = new System.Drawing.Size(340, 116);
+            this.Metin.TabIndex = 2;
+            this.Metin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Aranacak ifade:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Metin giriniz :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Turquoise;
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(791, 418);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Analiz Sonuçları";
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(547, 326);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 28);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "İleri";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 326);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(218, 28);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 326);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 29);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Geri";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(32, 15);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(698, 280);
+            this.textBox3.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tabPage3.Controls.Add(this.button8);
+            this.tabPage3.Controls.Add(this.button7);
+            this.tabPage3.Controls.Add(this.button6);
+            this.tabPage3.Controls.Add(this.listView1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(791, 418);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Loglama";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(535, 355);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(151, 26);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Temizle";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(292, 356);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(162, 29);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "<Geri";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(84, 355);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 30);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Ana Ekran";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(54, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(641, 308);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Programa hoş geldiniz ";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox Aranacakifade;
+        private System.Windows.Forms.TextBox Metin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+    }
+}
+
---
+++ Bil201odev/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bil201odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void kontrol() 
+        {
+
+            if (checkBox1.Checked == true)
+            {
+                if (Metin.Text.Contains(Aranacakifade.Text))
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Metin girin ");
+                } 
+            }
+            else if (checkBox2.Checked==true)
+            {
+                if (Metin.Text.Contains(Aranacakifade.Text))
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("lütfen sayı girin ");
+                }
+            }
+            else if (checkBox3.Checked ==true)
+            {
+                if (Metin.Text.Contains(Aranacakifade.Text))
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen karakter gir");
+                }
+            }
+             
+
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string metin = Metin.Text;
+            string aranacakIfade = Aranacakifade.Text;
+
+           
+
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int index = Metin.Text.IndexOf(Aranacakifade.Text);
+            textBox3.Text += " \r\n" + "Metin indexi : " + index.ToString();
+
+
+
+            kontrol();
+
+            int metinUzunlugu = Metin.Text.Length;
+            int aranacakifade = Aranacakifade.Text.Length;
+            textBox3.Text += " \r\n" + "Metin Uzunlugu : " + metinUzunlugu +   "\r\n" +"Aranacak ifade Uzunlugu : "+ aranacakifade;
+
+            ListViewItem item = new ListViewItem();
+            item.Text = $"{listView1.Items.Count + 1}. \"{Aranacakifade}\" metinde aranmış ve {MetinIcindeKacKez(Metin.Text, Aranacakifade.Text)} defa bulunmuştur. Metnin uzunluğu {metinUzunlugu} karakterdir.";
+            listView1.Items.Add(item);
+
+
+
+
+
+            
+        }
+        private int MetinIcindeKacKez(string metin, string Aranacakifade)
+        {
+            int adet = 0;
+            int index = -1;
+
+            while ((index = metin.IndexOf(Aranacakifade, index + 1)) != -1)
+            {
+                adet++;
+            }
+
+
+            return adet;
+        }
+
+            private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage3;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(1, "Programa hoş geldiniz", "Nesne yönelimli programlama ödevi 1", ToolTipIcon.Info);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Aranacakifade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
